aeea65bf169adeb48c03932eab9d0462d4a15253
Sheetal <sborar12@gmail.com>
2021-06-08 11:14:13 +0300

    load_data file

--- load_data.py
+++ load_data.py
@@ -1,29 +1,84 @@
-import os,gzip
-dataname = "mvision_train/img/image_testscan.npz"
-meta_dataname = "mvision_train/img/meta_testscan.pkz"
+import glob
+import gzip
+import os
+import re
+from PIL import Image
+from os.path import join, isdir
+import numpy as np
 
-data = np.load(dataname,allow_pickle=True)["arr_0"]
-meta_data = np.load(gzip.open(meta_dataname, 'rb'),allow_pickle=True)
+image_folder = None
+meta_folder = None
+mask_folder = None
 
-masksname = "mvision_train/mask_testscan"
-masks = []
+# use the meta name to put things in a certain folder
+base = "/azure-ml/mvinterns/deepmind-headneck-0/ct"
 
-for filename in os.listdir(masksname):
-    f = os.path.join(masksname, filename)
-    # checking if it is a file
-    mask = np.load(gzip.open(masksname + '/' + filename, 'rb'),allow_pickle=True)
-    masks.append(mask)
+image_dataname = "image*"
+meta_dataname = "meta*"
+mask_dataname = "mask*"
 
-shape = meta_data['shape']  # scan shape
-spacing = (meta_data['ND_SliceSpacing'], meta_data['PixelSpacing'][1], meta_data['PixelSpacing'][0])
+raw_data_folder = "raw_dataset"
+# raw_data_folder = "~/sheetal_project/src/Medical-Transformer/raw_dataset"
 
-roi_data = masks[0]
 
-shape = roi_data['shape']  # shape of scan
-bbox = roi_data['bbox']
-cropped_mask = roi_data['roi']
+# get all files with pattern
+image_files = glob.glob(join(base, image_dataname))
+meta_files = glob.glob(join(base, meta_dataname))
+mask_files = glob.glob(join(base, mask_dataname))
+# for each file
 
-mask = np.zeros(shape, dtype=np.bool)
+for image_file in image_files:
+    try:
+        pid_scanid = image_file.split('/')[-1][6:-4]
+        print(pid_scanid)
 
-b = [bbox[i] for i in [0, 3, 1, 4, 2, 5]]  # get it in (z_min, z_max, y_min, y_max, x_min, x_max)
-mask[b[0]: b[1], b[2]: b[3], b[4]: b[5]] = cropped_mask
+        meta_dataname = join(base,"meta_" + pid_scanid + ".pkz")
+        image = np.load(image_file, allow_pickle=True)["arr_0"]
+        meta_data = np.load(gzip.open(meta_dataname, 'rb'), allow_pickle=True)
+
+        masksname = join(base,"mask_" + pid_scanid +"/")
+        masks = []
+
+        for filename in os.listdir(masksname):
+            f = join(masksname, filename)
+            # checking if it is a file
+            mask = np.load(gzip.open(f, 'rb'), allow_pickle=True)
+            masks.append(mask)
+    except Error as e:
+        print(e)
+        continue
+
+    if not masks:
+        continue
+
+    shape = meta_data['shape']  # scan shape
+    spacing = (meta_data['ND_SliceSpacing'], meta_data['PixelSpacing'][1], meta_data['PixelSpacing'][0])
+
+    for mask_data in masks:
+
+        shape = mask_data['shape']  # shape of scan
+        bbox = mask_data['bbox']
+        cropped_mask = mask_data['roi']
+        body_part_name = mask_data['name']
+
+        mask = np.zeros(shape, dtype=np.bool)
+
+        b = [bbox[i] for i in [0, 3, 1, 4, 2, 5]]  # get it in (z_min, z_max, y_min, y_max, x_min, x_max)
+        mask[b[0]: b[1], b[2]: b[3], b[4]: b[5]] = cropped_mask
+
+        mask_rgb = (mask[:, :, :] * 255).astype(np.uint8)
+
+        body_part_folder = join(raw_data_folder, body_part_name)
+        if not isdir(body_part_folder):
+            os.makedirs(join(body_part_folder, "img"))
+            os.makedirs(join(body_part_folder, "labelcol"))
+
+        x = image.shape[0]
+
+        for i in range(x):
+            output_image_fname = join(body_part_folder, 'img', str(i)+ ".png")
+            output_label_fname = join(body_part_folder, 'labelcol', str(i)+".png")
+            Image.fromarray(np.uint8(image[i, :, :])).convert('RGB').resize((128, 128)).save(
+                output_image_fname)
+            Image.fromarray(np.uint8(mask_rgb[i, :, :])).convert('RGB').resize((128, 128)).save(
+                output_label_fname)
