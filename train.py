cfd188ae7cd0d69b5bfdb403b6db7f4a95612901
Sheetal <sborar12@gmail.com>
2021-06-22 21:47:49 +0800

    dice loss

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ parser.add_argument('--epochs', default=15, type=int, metavar='N',
                     help='number of total epochs to run(default: 400)')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch_size', default=2, type=int,
+parser.add_argument('-b', '--batch_size', default=30, type=int,
                     metavar='N', help='batch size (default: 1)')
 parser.add_argument('--learning_rate', default=1e-3, type=float,
                     metavar='LR', help='initial learning rate (default: 0.001)')
@@ -63,7 +63,7 @@ parser.add_argument('--direc', default='./medt', type=str,
                     help='directory to save')
 parser.add_argument('--crop', type=int, default=None)
 parser.add_argument('--imgsize', type=int, default=128)
-parser.add_argument('--device', default='cpu', type=str)
+parser.add_argument('--device', default='cuda', type=str)
 parser.add_argument('--gray', default='no', type=str)
 
 args = parser.parse_args()
